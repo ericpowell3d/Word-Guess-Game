88efe97952e54ff4ff562bc4849e3abc8d261886
epowell7 <ericpowell777@gmail.com>
2019-06-03 20:19:57 -0400

    almost fully functional, theme and css coming later

---
+++ README.md
@@ -0,0 +1,17 @@
+Description:
+
+Hangman of guessing character names from The Witcher game series
+
+Pseudo Code:
+
+-set a names array
+-get a random name from the array as the name to guess
+-find the amount of letters in the name and fill in with underscores
+-allow 6 guesses per name
+
+-get each letter from this name and set these letters as correct guesses
+-get a player guess and if it matches a letter, make only that letter visible and do not decrease the guess counter
+-append a series of the letters that the player has already guessed
+
+-if the player guesses all the letters, add a win and reset the name, amount of letters, amount of guesses, & letters guessed
+-if the player guess amount hits 0, add a loss and reset the name, amount of letters, amount of guesses, & letters guessed
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,155 @@
 $(document).ready(function() {
-  var names1 = [
-    "geralt", 
-    "vesemir", 
-    "triss", 
-    "lambert", 
-    "eskel", 
-    "shani", 
-    "foltest", 
-    "radovid", 
-    "dandelion", 
+  var names = [
     "abigail", 
+    "aldersberg", 
     "alvin", 
-    "javed", 
-    "vincent", 
-    "raymond", 
-    "professor", 
-    "reverend", 
-    "siegfried", 
-    "savolla", 
-    "thaler", 
-    "toruviel", 
-    "vaska", 
-    "velerad", 
-    "yaevinn", 
-    "zoltan", 
+    "annarietta", 
+    "avallach", 
     "berengar", 
     "carmen", 
-    "celina", 
-    "leuvaarden", 
+    "cecil", 
+    "cerys", 
+    "cirilla", 
+    "crach", 
+    "cynthia", 
+    "dandelion", 
+    "demavend", 
+    "dethmold", 
+    "dijkstra", 
+    "emhyr", 
+    "eredin", 
+    "ermion", 
+    "eskel", 
+    "foltest", 
+    "gaunter", 
+    "geralt", 
     "gramps", 
-    "ramsmeat", 
-    "jeanpierre", 
-    "jethro", 
+    "guillaume", 
+    "henselt", 
+    "hjalmar", 
+    "imlerith", 
+    "iorveth", 
+    "javed", 
+    "johnny", 
     "kalkstein", 
-    "eredin"];
+    "kiera", 
+    "lambert", 
+    "letho", 
+    "leuvaarden", 
+    "loredo", 
+    "nimue", 
+    "olgierd", 
+    "orianna", 
+    "phillip", 
+    "philippa", 
+    "priscilla", 
+    "professor", 
+    "radovid", 
+    "ramsmeat", 
+    "rayla", 
+    "raymond", 
+    "regis", 
+    "roche", 
+    "saskia", 
+    "savolla", 
+    "shani", 
+    "siegfried", 
+    "stennis", 
+    "syanna", 
+    "thaler", 
+    "toruviel", 
+    "triss", 
+    "vaska", 
+    "velerad", 
+    "vesemir", 
+    "vincent", 
+    "vivaldi", 
+    "vivienne", 
+    "vlodimir", 
+    "yaevinn", 
+    "yarpin", 
+    "yennefer", 
+    "zoltan"];
+
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var name = "";
+  var guessesRemaining = -1;
+  var lettersRemaining = name.length;
+
+  var wins = 0;
+  var losses = 0;
+
+  // Reset the game after a win/loss
+  function Reset() {
+    if (guessesRemaining > 0) {
+      wins++
+    }
+    else if (guessesRemaining == 0) {
+      losses++
+    }
+
+    $("#name").empty();
+    name = names[Math.floor(Math.random() * names.length)];
+
+    for (var i = 0; i < alphabet.length; i++) {
+      var letterBtn = $("<button>");
+      letterBtn.addClass("letter-button letter letter-button-color");
+      letterBtn.attr("id", alphabet[i]);
+      letterBtn.text(alphabet[i]);
+      $("#buttons").append(letterBtn);
+    }
+
+    for (var i = 0; i < name.length; i++){
+      var correct = $("<p>");
+      correct.attr("id", i);
+      correct.text(" _ ");
+      $("#name").append(correct);
+    }
     
-  var names2 = [
-    "", 
-    "", 
-    "",
-    "",
-    ""];
-    
-  var names3 = [
-    "", 
-    "", 
-    "",
-    "",
-    ""];
-  
-  
-  // var namesIndex2 = Math.floor(Math.random() * names2.length);
-  // var namesIndex3 = Math.floor(Math.random() * names3.length);
+    guessesRemaining = 6;
+    lettersRemaining = name.length;
 
-
-  // var nameSelect2 = names2 [nameIndex2];
-  // var nameSelect3 = names3 [nameIndex3];
-
-  function Select () {
-    var namesIndex1 = Math.floor(Math.random() * names1.length);
-    var nameSelect1 = names1 [nameIndex1];
+    console.log("Correct Name: " + name);
+    console.log("Remaining Guesses: " + guessesRemaining);
+    console.log("Remaining Letters: " + lettersRemaining);
+    console.log("Wins: " + wins);
+    console.log("Losses: " + losses);
+    console.log("");
   }
 
-  
+  Reset();
 
-  Select();
+  $(".letter-button").on("click", function() {
+    var userGuess = document.getElementById(this.id).id;
+    var correctGuess = false;
+    $("#" + userGuess).remove();
 
-  console.log(nameSelect1);
+    for (var i = 0; i < name.length; i++) {
+      if(userGuess === name[i].toUpperCase()) {
+        document.getElementById(i).textContent = " " + name[i].toUpperCase() + " ";
+        lettersRemaining--;
+        correctGuess = true;
+      }
+    }
+
+    if (!correctGuess) {
+      guessesRemaining--;
+    }
+
+    console.log("User Guessed: " + userGuess);
+    console.log("Remaining Guesses: " + guessesRemaining);
+    console.log("Remaining Letters: " + lettersRemaining);
+    console.log("");
+
+    correctGuess = false;
+
+    if (guessesRemaining == 0 || lettersRemaining == 0) {
+      for (var i = 0; i < alphabet.length; i++) {
+        $("#" + alphabet[i]).detach();
+      }
+      Reset();
+    }
+  });
 
 });
